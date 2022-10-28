17cb598a0a24f84d62dced230e32351ebf23e69c
Nick Carpenter <nick.carpenter@dotmatics.com>
2022-10-28 07:51:35 -0400

    Fixes on computer cleanup

--- src/main/java/io/jenkins/plugins/codebuildcloud/CodeBuildComputer.java
+++ src/main/java/io/jenkins/plugins/codebuildcloud/CodeBuildComputer.java
@@ -1,13 +1,10 @@
 package io.jenkins.plugins.codebuildcloud;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.concurrent.Future;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.Nonnull;
 
-import hudson.model.Computer;
 import hudson.model.Executor;
 import hudson.model.Queue;
 import hudson.slaves.AbstractCloudComputer;
@@ -39,63 +36,32 @@ public class CodeBuildComputer extends AbstractCloudComputer<CodeBuildSlave> {
     this.buildId = buildId;
   }
 
-  /**
-   * Gets a formatted AWS CodeBuild build URL.
-   *
-   * @return a {@link String} object.
-   */
-  public String getBuildUrl() {
-    try {
-      return String.format("https://%s.console.aws.amazon.com/codesuite/codebuild/projects/%s/build/%s",
-          cloud.getRegion(), cloud.getCodeBuildProjectName(), URLEncoder.encode(buildId, "UTF-8"));
-    } catch (UnsupportedEncodingException e) {
-      return buildId;
-    }
-  }
-
   /** {@inheritDoc} */
   @Override
   public void taskAccepted(Executor executor, Queue.Task task) {
     super.taskAccepted(executor, task);
-    LOGGER.info(String.format("[%s]: Task in job '%s' accepted", this, task.getFullDisplayName()));
+    LOGGER.log(Level.INFO, "[{0}]: JobName: {1}", new Object[] { this.getName(), task.getDisplayName() });
+    LOGGER.log(Level.INFO, "[{0}]: JobUrl: {1}", new Object[] { this.getName(), task.getUrl() });
+    LOGGER.log(Level.FINE, "[{0}]: taskAccepted", this);
   }
 
   /** {@inheritDoc} */
   @Override
   public void taskCompleted(Executor executor, Queue.Task task, long durationMS) {
     super.taskCompleted(executor, task, durationMS);
-    LOGGER.info(String.format("[%s]: Task in job '%s' completed in %sms", this, task.getFullDisplayName(), durationMS));
-    gracefulShutdown();
+    LOGGER.log(Level.FINE, "[{0}]: taskCompleted", this);
   }
 
   /** {@inheritDoc} */
   @Override
   public void taskCompletedWithProblems(Executor executor, Queue.Task task, long durationMS, Throwable problems) {
     super.taskCompletedWithProblems(executor, task, durationMS, problems);
-    LOGGER.severe(String.format("[%s]: Task in job '%s' completed with problems in %sms", this,
-        task.getFullDisplayName(), durationMS, problems));
-    gracefulShutdown();
+    LOGGER.log(Level.FINE, "[{0}]: taskCompletedWithProblems", this);
   }
 
   /** {@inheritDoc} */
   @Override
   public String toString() {
-    return String.format("name: %s buildID: %s", getName(), getBuildId());
-  }
-
-  private void gracefulShutdown() {
-    setAcceptingTasks(false);
-
-    Future<Object> next = Computer.threadPoolForRemoting.submit(() -> {
-      LOGGER.info(String.format("[%s]: Terminating agent after task.", this));
-      try {
-        Thread.sleep(500);
-        CodeBuildCloud.getJenkins().removeNode(getNode());
-      } catch (Exception e) {
-        LOGGER.info(String.format("[%s]: Termination error: %s", this, e.getClass()));
-      }
-      return null;
-    });
-    next.notify();
+    return String.format("name: %s buildID: %S Node: %s", getName(), getBuildId(), getNode());
   }
 }
